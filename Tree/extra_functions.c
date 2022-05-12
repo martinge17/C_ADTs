67a64ef3bac9a872225ef13d758bab845e4ec3ed
martinge17 <53666932+martinge17@users.noreply.github.com>
2022-05-12 18:30:21 +0200

    Added EsLleno and Test

--- Tree/extra_functions.c
+++ Tree/extra_functions.c
@@ -39,3 +39,122 @@ int height(tBinTree T){ //Max tree height
 	}
 }
 
+
+/* Check if tree is a "proper" tree (full tree)
+
+	- Every parent node/internal node has either two or no children.
+	- Leafs only at last level
+
+*/
+
+bool EsLleno_AUX(tBinTree T, int level, int height){
+
+	//Check if tree is empty
+	if(IsEmptyTree(T)){
+		return false;
+	}
+
+	//Leaf --> last level?
+	if(IsEmptyTree(T->left) && IsEmptyTree(T->right)){
+
+		return (level==height);
+	}
+
+	//Two childs --> Keep checking
+	if(!IsEmptyTree(T->left) && !IsEmptyTree(T->right)){
+
+		return (EsLleno_AUX(T->left,level+1,height) && EsLleno_AUX(T->right,level+1,height));
+	}
+
+	//One child only
+	return false;
+
+}
+
+bool EsLleno(tBinTree T){
+
+	return EsLleno_AUX(T,1,height(T));
+}
+
+
+
+/*  >------------------------------------------------------ TEST ------------------------------------------------------< */
+
+/*             Original Tree
+ * 					100
+ *			   24         42
+ *			1      1   2      32
+ */
+
+
+
+int main(){
+
+    tBinTree main_tree;
+    CreateEmptyTree(&main_tree);
+    tBinTree tree_2;
+    CreateEmptyTree(&tree_2);
+    tBinTree tree_3;
+    CreateEmptyTree(&tree_3);
+    tBinTree tree_4;
+    CreateEmptyTree(&tree_4);
+    tBinTree tree_5;
+    CreateEmptyTree(&tree_5);
+    tBinTree tree_6;
+    CreateEmptyTree(&tree_6);
+    tBinTree tree_7;
+    CreateEmptyTree(&tree_7);
+
+
+    BuildTree(TNULL,2,TNULL,&tree_6);
+    BuildTree(TNULL,32,TNULL,&tree_7);
+
+    BuildTree(tree_6,42,tree_7,&tree_3);
+
+    BuildTree(TNULL,1,TNULL,&tree_4);
+    BuildTree(TNULL,1,TNULL,&tree_5);
+
+    BuildTree(tree_4,24,tree_5,&tree_2);
+
+    BuildTree(tree_2,100,tree_3,&main_tree);
+
+
+	//Test Node Counter
+	printf("Num of Nodes: %d\n",countNodes(main_tree));
+
+	//Test Leaf
+	printf("Num of Leafs: %d\n",countL(main_tree));
+
+	//Test Height
+	printf("Height: %d\n",height(main_tree));
+
+	//Check full tree
+    if (EsLleno(main_tree))
+    {
+        printf("Full Tree!\n");
+    }else{
+        printf("No Full Tree!\n");
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
