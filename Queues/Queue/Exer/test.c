9db51f790cd600d17394159933c38b79b13c1c05
martinge17 <53666932+martinge17@users.noreply.github.com>
2022-07-14 11:23:35 +0200

    Update Test

--- Queues/Queue/Exer/test.c
+++ Queues/Queue/Exer/test.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include "dynamic_queue.h"
-//#include "static_queue.h"
+#include "circular_queue.h"
+
 
 void print_front(tQueue Q){
-    printf(" %d ",front(Q));
+    if(!isEmptyQueue(Q)){
+        printf(" %d \n",front(Q));
+    }else{
+        printf("Empty!\n");
+    }
 }
 
 int main(){
@@ -11,16 +15,31 @@ int main(){
     tQueue Queue;
     createEmptyQueue(&Queue);
     enqueue(1,&Queue);
+    if(!isEmptyQueue(Queue)){ dequeue(&Queue); }
     enqueue(3,&Queue);
-    dequeue(&Queue);
+    print_front(Queue);
+    if(!isEmptyQueue(Queue)){ dequeue(&Queue); }
     print_front(Queue);
     enqueue(15,&Queue);
     enqueue(20,&Queue);
+    if(!isEmptyQueue(Queue)){ dequeue(&Queue); }
     print_front(Queue);
-    dequeue(&Queue);
-    //dequeue(&Queue);
-    //dequeue(&Queue);
+    if(!isEmptyQueue(Queue)){ dequeue(&Queue); }
     print_front(Queue);
-    
+    if(!isEmptyQueue(Queue)){ dequeue(&Queue); }
+    print_front(Queue); //Empty
+    if(!isEmptyQueue(Queue)){ dequeue(&Queue); }
+    print_front(Queue); //Empty
+
+    printf("Check\n");
+
+
+    //Check if full
+    if(!enqueue(1,&Queue)){
+        printf("Full!!!!\n");
+    }else{
+        printf("No problem\n");
+    }
+
     return 0;
 }
